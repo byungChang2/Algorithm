b016d5a68564ee3164b0f87b7a63f67b59be3a3f
ByungChang <govlqudckd@gmail.com>
2022-02-18 15:31:32 +0900

    1

--- codeup81~90/codeup83.py
+++ codeup81~90/codeup83.py
@@ -16,9 +16,9 @@ for i in range(1, n+1) :
 '''
 a = int(input())
 for i in range(1,a+1):
-  if i%10==3:
-    print("x",end='')
+  if i%10==3 or i%10==6 or i%10 ==9:
+    print("X",end=' ')
   else:
-    print(i,end='')
+    print(i,end=' ')
 
     
